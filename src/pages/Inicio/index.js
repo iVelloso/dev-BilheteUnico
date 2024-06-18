b7744531c50df076bf53c3e777cc19731e50709f
juliamfr <161901285+juliamfr@users.noreply.github.com>
2024-06-18 18:27:42 -0300

    Add files via upload

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 
 export default function Inicio() {
+  const image1 = { uri: 'assets/bilheteinicio.png' };
+  const image2 = { uri: 'assets/unicoinicio.png' };
+  const image3 = { uri: 'assets/trevo.png' };
+  
   return (
-    <View style={styles.container} /> 
+    <View style={styles.container}>
+      <Image source={image1} style={styles.image1} />
+      <Image source={image2} style={styles.image2} />
+      <Image source={image3} style={styles.image3} />
+    </View>
   );
 }
 
@@ -11,5 +19,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#087C57',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image1: {
+    width: 300, 
+    height: 200, 
+    resizeMode: 'contain',
+    marginBottom: 50,
+    marginTop: -200,
+ },
+  image2: {
+    width: 200, 
+    height: 200, 
+    resizeMode: 'contain',
+    marginBottom: 50,
+    marginTop: -170,
+  },
+  image3: {
+    width: 350, 
+    height: 350, 
+    resizeMode: 'contain',
+    position: 'absolute',
+    bottom: 50,
+    right: -25,
   },
 });
